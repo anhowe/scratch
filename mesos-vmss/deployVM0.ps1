09eabf5a901a60c8aaa5de8cf96a925feeabc36d
anhowe <ahowe_ca@yahoo.ca>
2016-01-13 16:42:07 -0800

    adding first cut of template

---
+++ mesos-vmss/deployVM0.ps1
@@ -0,0 +1,9 @@
+$VerbosePreference="Continue"
+$deployName="aaajmgmt0113a"
+$RGName=$deployName
+$locName="East Asia"
+$templateFile= "mesos-cluster.json"
+$templateParameterFile= "cluster.parameters.json"
+New-AzureRmResourceGroup -Name $RGName -Location $locName -Force
+
+New-AzureRmResourceGroupDeployment -Name $deployName -ResourceGroupName $RGName -TemplateParameterFile $templateParameterFile -TemplateFile $templateFile
