e7b4df575c68b0b1364f031c31841f6669097fa9
josrom <josrom@microsoft.com>
2016-04-14 21:24:23 -0700

    Swarm Windows Template Updates Based on Code Review Feedback
    Changed Mesos and Linux to Swarm and Windows in template help where
    applicable.
    Write Output of CustomDataSetupScript to log file on Windows VMs
    Add comments to WindowsDockerDeploy script

--- mesos-marathon-vmss/parts/WindowsDockerDeploy.ps1
+++ mesos-marathon-vmss/parts/WindowsDockerDeploy.ps1
@@ -3,18 +3,23 @@
         Copyright (c) Microsoft Corporation.  All rights reserved.
 
     .SYNOPSIS
-        Updates a container host with latest docker.exe
+        Updates a Windows Server Docker host with latest docker.exe and joins it to a Docker Swarm
 
     .DESCRIPTION
-        Collects collateral required to create a container host
-        Creates a VM
-        Configures the VM as a new container host
+        Stop Docker
+        Opens TCP ports (80,443,2375,8080) in Windows Firewall.
+        Updates Windows Docker Binary from DockerPath
+        Rewrites runDockerDaemon.cmd startup script to join a Docker Swarm
+        Start Docker
 
     .PARAMETER DockerPath
         Path to a private Docker.exe.  Defaults to https://aka.ms/tp4/docker
 
+    .PARAMETER SwarmMasterIP
+        IP Address of Docker Swarm Master
+
     .EXAMPLE
-        .\Update-ContainerHost.ps1 -SkipDocker
+        .\WindowsDockerDeploy.ps1 
 
 #>
 #Requires -Version 5.0
@@ -31,8 +36,8 @@ param(
     $SwarmMasterIP = "172.16.0.5"
 )
 
-function
-Update-ContainerHost()
+# Stops Docker, Update FireWall Rules, Adds Cluster Info To Docker Config Scrpt, Restart Docker
+function Update-ContainerHost()
 {
     Test-Admin
 
@@ -55,7 +60,7 @@ Update-ContainerHost()
     #
     # Write Docker Script
     #
-    Write-DockerStartupScript
+    Write-DockerStartupScriptWithSwarmClusterInfo
 
     #
     # Update service
@@ -71,13 +76,14 @@ Update-ContainerHost()
 $global:AdminPriviledges = $false
 $global:DockerServiceName = "Docker"
 
+# Get Node IPV4 Address
 function Get-IPAddress()
 {
     return (Get-NetIPAddress | where {$_.IPAddress -Like '10.*'})[0].IPAddress
 }
 
-function
-Open-FirewallPorts()
+# Open Windows Firewall Ports Needed
+function Open-FirewallPorts()
 {
     $ports = @(80,443,2375,8080)
     foreach ($port in $ports)
@@ -88,8 +94,8 @@ Open-FirewallPorts()
     }
 }
 
-function
-Write-DockerStartupScript()
+# Update Docker Config to have cluster-store=consul:// address configured for Swarm cluster.
+function Write-DockerStartupScriptWithSwarmClusterInfo()
 {
     $dataDir = $env:ProgramData
 
@@ -111,8 +117,8 @@ Write-DockerStartupScript()
     $OutFile | Out-File -encoding ASCII -filepath "$targetDir\runDockerDaemon.cmd"
 }
 
-function
-Copy-File
+# Copy a file from local filesystem or Internet
+function Copy-File
 {
     [CmdletBinding()]
     param(
@@ -180,110 +186,8 @@ Copy-File
     }
 }
 
-
-function
-Expand-ArchiveNano
-{
-    [CmdletBinding()]
-    param
-    (
-        [string] $Path,
-        [string] $DestinationPath
-    )
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($Path, $DestinationPath)
-}
-
-
-function
-Expand-ArchivePrivate
-{
-    [CmdletBinding()]
-    param
-    (
-        [Parameter(Mandatory=$true)]
-        [string]
-        $Path,
-
-        [Parameter(Mandatory=$true)]
-        [string]
-        $DestinationPath
-    )
-
-    $shell = New-Object -com Shell.Application
-    $zipFile = $shell.NameSpace($Path)
-
-    $shell.NameSpace($DestinationPath).CopyHere($zipFile.items())
-
-}
-
-
-function
-Get-InstalledContainerImage
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]
-        [ValidateNotNullOrEmpty()]
-        $BaseImageName
-    )
-
-    return Get-ContainerImage |? IsOSImage |? Name -eq $BaseImageName
-}
-
-
-function
-Get-Nsmm
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]
-        [ValidateNotNullOrEmpty()]
-        $Destination,
-
-        [string]
-        [ValidateNotNullOrEmpty()]
-        $WorkingDir = "$env:temp"
-    )
-
-    Write-Output "This script uses a third party tool: NSSM. For more information, see https://nssm.cc/usage"
-    Write-Output "Downloading NSSM..."
-
-    $nssmUri = "https://nssm.cc/release/nssm-2.24.zip"
-    $nssmZip = "$($env:temp)\$(Split-Path $nssmUri -Leaf)"
-
-    Write-Verbose "Creating working directory..."
-    $tempDirectory = New-Item -ItemType Directory -Force -Path "$($env:temp)\nssm"
-
-    Copy-File -SourcePath $nssmUri -DestinationPath $nssmZip
-
-    Write-Output "Extracting NSSM from archive..."
-    if (Test-Nano)
-    {
-        Expand-ArchiveNano -Path $nssmZip -DestinationPath $tempDirectory.FullName
-    }
-    elseif ($PSVersionTable.PSVersion.Major -ge 5)
-    {
-        Expand-Archive -Path $nssmZip -DestinationPath $tempDirectory.FullName
-    }
-    else
-    {
-        Expand-ArchivePrivate -Path $nssmZip -DestinationPath $tempDirectory.FullName
-    }
-    Remove-Item $nssmZip
-
-    Write-Verbose "Copying NSSM to $Destination..."
-    Copy-Item -Path "$($tempDirectory.FullName)\nssm-2.24\win64\nssm.exe" -Destination "$Destination"
-
-    Write-Verbose "Removing temporary directory..."
-    $tempDirectory | Remove-Item -Recurse
-}
-
-
-function
-Test-Admin()
+# Are We Runing as Admin?
+function Test-Admin()
 {
     # Get the ID and security principal of the current user account
     $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -308,67 +212,16 @@ Test-Admin()
     }
 }
 
-
-function
-Test-ContainerProvider()
-{
-    if (-not (Get-Command Install-ContainerImage -ea SilentlyContinue))
-    {
-        Wait-Network
-
-        Write-Output "Installing ContainerProvider package..."
-        Install-PackageProvider ContainerProvider -Force | Out-Null
-    }
-
-    if (-not (Get-Command Install-ContainerImage -ea SilentlyContinue))
-    {
-        throw "Could not install ContainerProvider"
-    }
-}
-
-
-function
-Test-Nano()
+# Are We Running on Nano Server?
+function Test-Nano()
 {
     $EditionId = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name 'EditionID').EditionId
 
     return (($EditionId -eq "NanoServer") -or ($EditionId -eq "ServerTuva"))
 }
 
-
-function
-Wait-Network()
-{
-    $connectedAdapter = Get-NetAdapter |? ConnectorPresent
-
-    if ($connectedAdapter -eq $null)
-    {
-        throw "No connected network"
-    }
-
-    $startTime = Get-Date
-    $timeElapsed = $(Get-Date) - $startTime
-
-    while ($($timeElapsed).TotalMinutes -lt 5)
-    {
-        $readyNetAdapter = $connectedAdapter |? Status -eq 'Up'
-
-        if ($readyNetAdapter -ne $null)
-        {
-            return;
-        }
-
-        Write-Output "Waiting for network connectivity..."
-        Start-Sleep -sec 5
-
-        $timeElapsed = $(Get-Date) - $startTime
-    }
-
-    throw "Network not connected after 5 minutes"
-}
-
-function
-Wait-DockerInstallation()
+# Wait For Docker Installation
+function Wait-DockerInstallation()
 {
     $expiryMinutes = 10
     $startTime = Get-Date
@@ -392,23 +245,8 @@ Wait-DockerInstallation()
     throw "Docker did not get installed after $expiryMinutes minutes"
 }
 
-function
-Find-DockerImages
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]
-        [ValidateNotNullOrEmpty()]
-        $BaseImageName
-    )
-
-    return docker images | Where { $_ -match $BaseImageName.tolower() }
-}
-
-
-function
-Start-Docker()
+# Start Docker
+function Start-Docker()
 {
     Write-Output "Starting $global:DockerServiceName..."
     if (Test-Nano)
@@ -422,8 +260,8 @@ Start-Docker()
 }
 
 
-function
-Stop-Docker()
+# Stop Docker
+function Stop-Docker()
 {
     Write-Output "Stopping $global:DockerServiceName..."
     if (Test-Nano)
@@ -441,9 +279,8 @@ Stop-Docker()
     }
 }
 
-
-function
-Test-Docker()
+# Is Docker Installed?
+function Test-Docker()
 {
     $service = $null
 
@@ -459,9 +296,8 @@ Test-Docker()
     return ($service -ne $null)
 }
 
-
-function
-Wait-Docker()
+# Wait For Docker To Start
+function Wait-Docker()
 {
     Write-Output "Waiting for Docker daemon..."
     $dockerReady = $false
@@ -501,65 +337,6 @@ Wait-Docker()
         }
     }
     Write-Output "Successfully connected to Docker Daemon."
-}
-
-
-function
-Write-DockerImageTag()
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]
-        $BaseImageName
-    )
-
-    $dockerOutput = Find-DockerImages $BaseImageName
-
-    if ($dockerOutput.Count -gt 1)
-    {
-        Write-Output "Base image is already tagged:"
-    }
-    else
-    {
-        if ($dockerOutput.Count -lt 1)
-        {
-            #
-            # Docker restart required if the image was installed after Docker was
-            # last started
-            #
-            Stop-Docker
-            Start-Docker
-
-            $dockerOutput = Find-DockerImages $BaseImageName
-
-            if ($dockerOutput.Count -lt 1)
-            {
-                throw "Could not find Docker image to match '$BaseImageName'"
-            }
-        }
-
-        if ($dockerOutput.Count -gt 1)
-        {
-            Write-Output "Base image is already tagged:"
-        }
-        else
-        {
-            #
-            # Register the base image with Docker
-            #
-            $imageId = ($dockerOutput -split "\s+")[2]
-
-            Write-Output "Tagging new base image ($imageId)..."
-
-            docker tag $imageId "$($BaseImageName.tolower()):latest"
-            Write-Output "Base image is now tagged:"
-
-            $dockerOutput = Find-DockerImages $BaseImageName
-        }
-    }
-
-    Write-Output $dockerOutput
 }
 
 try
